2b0c0d5c74e13881a08f5fdad2c15ba75cea0ff4
Pavel Zhdanovich <swatoplus@gmail.com>
2020-10-31 15:36:25 +0300

    Added FullTextSearch

---
+++ FullTextSearch/FullTextSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FullTextSearch
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
