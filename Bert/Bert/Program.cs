a019e82c66a779d6f399b69f3df390add33c1201
Pavel Zhdanovich <swatoplus@gmail.com>
2020-11-21 17:42:58 +0300

    Added Bert

---
+++ Bert/Bert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
